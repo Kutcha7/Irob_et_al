a34051df55393e8bfd2268466a385d84d41c8137
= <=>
2021-06-09 18:07:54 +0200

    Fix double package calls

--- Analysis/Composition.R
+++ Analysis/Composition.R
@@ -8,18 +8,16 @@
 rm(list=ls()) # clears working environment 
 
 
-library(tidyr)
-library(dplyr)
+library(tidyverse)
 options(dplyr.width = Inf) #enables head() to display all coloums
-library(ggplot2)
 library(grid)
 library(gridExtra)
 library(reshape2)
-library(tidyverse)
 library(scales)
 library(gtable)
 library(cowplot)
 library(data.table)
+library(cowplot)
 
 # setting working directory --
 paths <- here::here("Data/Results/")
@@ -282,7 +280,6 @@ perennials_by_strat_bytreatment_swap <- perennials_by_strat_bytreatment_swap + t
 shrubs_by_strat_bytreatment_swap <- shrubs_by_strat_bytreatment_swap + theme(legend.position="none")
 
 
-library(cowplot)
 
 compplots <- plot_grid(perennials_by_strat_bytreatment_swap, shrubs_by_strat_bytreatment_swap,
                         ncol=2,  nrow=1, 
